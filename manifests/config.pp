7c6fda92ebedb51e3c636a73d6607f96a087858b
Adam Breaux <breauxa@email.chop.edu>
2015-05-01 13:44:33 -0400

    wip

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,11 @@
 define phpmssql::config (
-
+  $global_tds_version,
+  $global_dump_file,
+  $global_debug_flags,
+  $global_timeout,
+  $global_connect_timeout,
+  $global_text_size,
+  $servers = '',
 ) {
   $required = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'php-mssql' ],
